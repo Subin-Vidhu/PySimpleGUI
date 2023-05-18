5286d13799ca99adae3163238a8862ee8b47955e
Subin-Vidhu <85066028+Subin-Vidhu@users.noreply.github.com>
2023-05-18 06:25:17 +0530

    4

--- Create a GUI with Python/4_example._user_inputpy.py
+++ Create a GUI with Python/4_example._user_inputpy.py
@@ -2,11 +2,11 @@ import PySimpleGUIQt as sg
 
 form = sg.FlexForm('My first GUI')
 
-layout = [ [sg.Text('Enter your name', size=(15, 1)), sg.InputText()],
-           [sg.Text('Enter your country', size=(15, 1)), sg.InputText()],
-           [sg.Text('Enter your phone', size=(15, 1)), sg.InputText()],
-           [sg.Text('Enter your city', size=(15, 1)), sg.InputText()],
-           [sg.OK(), sg.Cancel()] ]
+layout = [[sg.Text('Enter your name', size=(15, 1)), sg.InputText()],
+          [sg.Text('Enter your country', size=(15, 1)), sg.InputText()],
+          [sg.Text('Enter your phone', size=(15, 1)), sg.InputText()],
+          [sg.Text('Enter your city', size=(15, 1)), sg.InputText()],
+          [sg.OK(), sg.Cancel()]]
 
 button, values = form.Layout(layout).Read()
 name = values[0]
@@ -14,4 +14,4 @@ country = values[1]
 phone = values[2]
 city = values[3]
 
-print(f"name {name}, country {country}, phone {phone}, city {city}")
+print(f" name - {name}\n country - {country}\n phone - {phone}\n city - {city}")
